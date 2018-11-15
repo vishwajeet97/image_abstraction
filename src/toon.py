c8bdb068c86f84fb3630e53ec978880841d7b1b0
Kartik Singhal <kartiks025@gmail.com>
2018-11-15 23:49:08 +0530

    using scipy lic

--- src/toon.py
+++ src/toon.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import lic.lic_internal
+
 class Toon:
 	def __init__(self, image, config):
 		self.image = image
@@ -10,9 +12,12 @@ class Toon:
 		sobelx = cv2.Sobel(gray_img,cv2.CV_64F,1,0,ksize=5)
 		sobely = cv2.Sobel(gray_img,cv2.CV_64F,0,1,ksize=5)
 		mag = np.sqrt(sobelx**2 + sobely**2)
-		scale_factor = np.max(mag)/255
-		mag = (mag/scale_factor).astype(np.uint8)
 		KERNEL_SIZE = 5
+		texture = np.random.rand(sobelx.shape(0),sobelx.shape(1)).astype(np.float32)
+		kernellen=31
+		kernel = np.sin(np.arange(kernellen)*np.pi/kernellen)
+		kernel = kernel.astype(np.float32)
+		lic_internal.line_integral_convolution(np.stack(sobelx,sobely,axis=2), texture, kernel)
 		return sobely
 
 	def run(self):
