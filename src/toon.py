a3cde399293d160f78f91d4f704d6b5bd359bf34
Kartik Singhal <kartiks025@gmail.com>
2018-11-15 21:51:54 +0530

    modififying gradient calc

--- src/toon.py
+++ src/toon.py
@@ -1,16 +1,22 @@
+import cv2
+import numpy as np
 class Toon:
 	def __init__(self, image, config):
 		self.image = image
 		self.config = config
 
-	def ETF(image):
-		gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		sobelx = cv.Sobel(gray_img,cv2.CV_64F,1,0,ksize=5)
-		sobely = cv.Sobel(gray_img,cv2.CV_64F,0,1,ksize=5)
-		sobelx = np.uint8(np.absolute(sobelx))
-		sobely = np.uint8(np.absolute(sobely))
-		mag, ang = cv2.cartToPolar(sobelx, sobely)
+	def ETF(self):
+		gray_img = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+		sobelx = cv2.Sobel(gray_img,cv2.CV_64F,1,0,ksize=5)
+		sobely = cv2.Sobel(gray_img,cv2.CV_64F,0,1,ksize=5)
+		mag = np.sqrt(sobelx**2 + sobely**2)
+		scale_factor = np.max(mag)/255
+		mag = (mag/scale_factor).astype(np.uint8)
 		KERNEL_SIZE = 5
-		
+		return sobely
+
+	def run(self):
+		etf = self.ETF()
+		return etf
 
 
