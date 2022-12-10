f606f0d1fad1c3dd0b4f0fa8e4e33e953672f6f3
ATblackwhite <1621864188@qq.com>
2022-12-10 18:15:03 +0800

    modify the running environment
    Enable the code run in WIN10

--- src/vectorplot/setup.py
+++ src/vectorplot/setup.py
@@ -1,11 +1,16 @@
 from Cython.Build import cythonize
-
-def configuration(parent_package='',top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('vectorplot', parent_package, top_path)
-
-    #config.add_extension("lic_internal", ["lic_internal.pyx"],
-    #                     language='cython')
-    #config.add_subpackage('tests')
-
-    return config
+from distutils.core import setup
+import numpy as np
+# def configuration(parent_package='',top_path=None):
+#     from numpy.distutils.misc_util import Configuration
+#     config = Configuration('vectorplot', parent_package, top_path)
+#
+#     #config.add_extension("lic_internal", ["lic_internal.pyx"],
+#     #                     language='cython')
+#     #config.add_subpackage('tests')
+#
+#     return config
+setup(
+    ext_modules=cythonize("lic_internal.pyx"),
+    include_dirs=[np.get_include()]
+)
